a025dade92efd776dab40d7362746233132052be
unknown <willianantonioabrego@gmail.com>
2026-06-10 16:16:49 -0600

    agregado app.js, users y dashboard

---
+++ js/app.js
@@ -0,0 +1,12 @@
+function loadPage(page) {
+  fetch(`pages/${page}.html`)
+    .then((response) => response.text())
+    .then((html) => {
+      document.getElementById("content").innerHTML = html;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+loadPage("dashboard");
